70bad44e1de95f70162d274230d0e51e647e7c01
Alex Fernando <alexdevreactjs23@gmail.com>
2023-09-11 14:46:41 -0300

    Feat: Adicionando Estilos através do atributo styles

--- App.tsx
+++ App.tsx
@@ -2,9 +2,25 @@ import { View, Text } from 'react-native'
 
 export default function App () {
   return (
-    <View>
-      <Text>React</Text>
-      <Text>Native</Text>
+    <View style = {{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}>
+      <Text  style = {{
+        color: '#FFF',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}>
+        Nome do Evento
+      </Text>
+      <Text style = {{
+        color: '#6B6B6B',
+        fontSize: 16
+      }}>
+        Segunda-feira, 11 de Setembro de 2023
+      </Text>
     </View>
   )
 }
